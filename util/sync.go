6739780d4faed9bff160599b2e10f42b4cac60e1
yangwei <ywengineer@gmail.com>
2019-12-03 14:49:17 +0800

    * watch context

---
+++ util/sync.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"context"
+	"time"
+)
+
+func Watch(ctx context.Context, notify chan<- bool) {
+	go func() {
+		ticker := time.NewTicker(500 * time.Millisecond)
+	stop:
+		for {
+			select {
+			case <-ctx.Done():
+				Info("terminating: context cancelled")
+				notify <- true
+				break stop
+			case <-ticker.C:
+			}
+		}
+		ticker.Stop()
+	}()
+}
