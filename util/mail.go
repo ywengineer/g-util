79e1964551cec668e4a819d1e837bde24ad7045c
yangwei <ywengineer@gmail.com>
2019-12-10 13:52:53 +0800

    * add mail client and global mail client

--- util/mail.go
+++ util/mail.go
@@ -4,12 +4,77 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
-var client gomail.SendCloser
+type MailClient struct {
+	client gomail.SendCloser
+}
 
-func Dial(host string, port int, username, password string) {
+func (mc *MailClient) SendMail(from, to, cc, bcc string, subject, bodyType, bodyString string) {
+	var rec []string
+	if ValidMail(from) == false {
+		Error("missing mail's sender")
+		return
+	}
+	if ValidMail(to) == false {
+		Error("missing mail's to")
+		return
+	}
+	rec = append(rec, to)
+	//
+	if len(cc) > 0 {
+		if ValidMail(cc) == false {
+			Error("unknown mail's cc. %s", cc)
+			return
+		}
+		rec = append(rec, to)
+	}
+
+	//
+	if len(bcc) > 0 {
+		if ValidMail(bcc) == false {
+			Error("unknown mail's bcc. %s", bcc)
+			return
+		}
+		rec = append(rec, bcc)
+	}
+	//
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	if len(cc) > 0 {
+		m.SetAddressHeader("Cc", cc, cc)
+	}
+	if len(bcc) > 0 {
+		m.SetAddressHeader("Bcc", bcc, bcc)
+	}
+	m.SetHeader("Subject", subject)
+	m.SetBody(bodyType, bodyString)
+	// Send the email to Bob, Cora and Dan.
+	if err := mc.client.Send(from, rec, m); err != nil {
+		Error("send mail failed, %v", err)
+	}
+}
+
+//var client gomail.SendCloser
+
+func NewMailSender(host string, port int, username, password string) (*MailClient, error) {
 	d := gomail.NewDialer(host, port, username, password)
 	if c, err := d.Dial(); err != nil {
-		Panic("create mail client failed. %v", err)
+		return nil, err
+	} else {
+		return &MailClient{client: c}, nil
+	}
+}
+
+var client *MailClient
+
+func Dial(host string, port int, username, password string) {
+	if client != nil {
+		Error("global mail client already exists.")
+		return
+	}
+	//
+	if c, e := NewMailSender(host, port, username, password); e != nil {
+		Panic("create global mail client failed. %v", e)
 	} else {
 		client = c
 	}
@@ -17,52 +82,10 @@ func Dial(host string, port int, username, password string) {
 
 func SendMail(from, to, cc, bcc string, subject, bodyType, bodyString string) {
 	if client == nil {
-		Error("missing mail client")
-	} else {
-		var rec []string
-		if ValidMail(from) == false {
-			Error("missing mail's sender")
-			return
-		}
-		if ValidMail(to) == false {
-			Error("missing mail's to")
-			return
-		}
-		rec = append(rec, to)
-		//
-		if len(cc) > 0 {
-			if ValidMail(cc) == false {
-				Error("unknown mail's cc. %s", cc)
-				return
-			}
-			rec = append(rec, to)
-		}
-
-		//
-		if len(bcc) > 0 {
-			if ValidMail(bcc) == false {
-				Error("unknown mail's bcc. %s", bcc)
-				return
-			}
-			rec = append(rec, bcc)
-		}
-		//
-		m := gomail.NewMessage()
-		m.SetHeader("From", from)
-		m.SetHeader("To", to)
-		if len(cc) > 0 {
-			m.SetAddressHeader("Cc", cc, cc)
-		}
-		if len(bcc) > 0 {
-			m.SetAddressHeader("Bcc", bcc, bcc)
-		}
-		m.SetHeader("Subject", subject)
-		m.SetBody(bodyType, bodyString)
-		// Send the email to Bob, Cora and Dan.
-		if err := client.Send(from, rec, m); err != nil {
-			Error("send mail failed, %v", err)
-		}
+		Error("global mail client has not been created.")
+		return
 	}
+	client.SendMail(from, to, cc, bcc, subject, bodyType, bodyString)
 }
 
 func DirectSendMail(host string, port int, username, password string,
