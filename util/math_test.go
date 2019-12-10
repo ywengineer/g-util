aa6f3e5b5af1f888fd8c46529f723e3bc03b63d9
yangwei <ywengineer@gmail.com>
2019-12-10 14:39:51 +0800

    * add test case

--- util/math_test.go
+++ util/math_test.go
@@ -1,6 +1,9 @@
 package util
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMath(t *testing.T) {
 	t.Logf("%v\n", ValidMail("ywengineer@gmail.com"))
@@ -9,4 +12,12 @@ func TestMath(t *testing.T) {
 	t.Logf("%v\n", ValidMail("iakf_ie.12@gmail.cn"))
 	t.Logf("%v\n", ValidMail("iakf_ie.12@gmail.online"))
 	t.Logf("%v\n", ValidMail("iakf_ie.12@gmail.store"))
+}
+
+func TestMap(t *testing.T) {
+	m := []map[string]interface{}{
+		{"a": 1},
+		{"b": 1},
+	}
+	t.Log(reflect.TypeOf(m[0]).Kind().String())
 }
