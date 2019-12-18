076b782703dc50165313c0d903cf72e8c6edca76
yangwei <ywengineer@gmail.com>
2019-12-18 15:17:04 +0800

    * add method WatchQuitSignal

--- util/sync.go
+++ util/sync.go
@@ -2,8 +2,23 @@ package util
 
 import (
 	"context"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+func WatchQuitSignal() chan os.Signal {
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGTSTP)
+	//
+	return quit
+}
 
 func Watch(ctx context.Context, notify chan<- bool) {
 	go func() {
