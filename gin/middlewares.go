ca669a4ffa4054ea9fdd2a82628187bc687a00d6
yangwei <ywengineer@gmail.com>
2020-05-09 11:05:10 +0800

    * upgrade g-util

--- gin/middlewares.go
+++ gin/middlewares.go
@@ -1,11 +1,13 @@
 package gin
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/openzipkin/zipkin-go"
 	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
 	"github.com/openzipkin/zipkin-go/model"
+	"github.com/openzipkin/zipkin-go/propagation/b3"
 	"github.com/ywengineer/g-util/api"
 	"github.com/ywengineer/g-util/cache"
 	"net/http"
@@ -14,20 +16,28 @@ import (
 )
 
 func MiddlewareZipKin(tracer *zipkin.Tracer) gin.HandlerFunc {
-	return func(context *gin.Context) {
-		span, ctx := tracer.StartSpanFromContext(context.Request.Context(), context.FullPath(), zipkin.Kind(model.Server))
-		defer span.Finish()
-		context.Request = context.Request.WithContext(ctx)
-
-		zipkin.TagHTTPMethod.Set(span, context.Request.Method)
-		zipkin.TagHTTPPath.Set(span, context.Request.URL.Path)
-		zipkin.TagHTTPRequestSize.Set(span, fmt.Sprintf("%d", context.Request.ContentLength))
+	return func(gc *gin.Context) {
+		parent := tracer.Extract(b3.ExtractHTTP(gc.Request))
+		var span zipkin.Span
+		// no parent span
+		if parent.Err == nil {
+			var ctx context.Context
+			span, ctx = tracer.StartSpanFromContext(gc.Request.Context(), gc.FullPath(), zipkin.Kind(model.Server))
+			defer span.Finish()
+			gc.Request = gc.Request.WithContext(ctx)
+		} else {
+			span = tracer.StartSpan(gc.FullPath(), zipkin.Parent(parent), zipkin.Kind(model.Server))
+			defer span.Finish()
+		}
+		zipkin.TagHTTPMethod.Set(span, gc.Request.Method)
+		zipkin.TagHTTPPath.Set(span, gc.Request.URL.Path)
+		zipkin.TagHTTPRequestSize.Set(span, fmt.Sprintf("%d", gc.Request.ContentLength))
 		//
-		context.Next()
+		gc.Next()
 		//
-		if statusCode := context.Writer.Status(); statusCode < 200 || statusCode > 299 {
-			zipkin.TagHTTPStatusCode.Set(span, strconv.Itoa(context.Writer.Status()))
-			zipkin.TagHTTPResponseSize.Set(span, strconv.Itoa(context.Writer.Size()))
+		if statusCode := gc.Writer.Status(); statusCode < 200 || statusCode > 299 {
+			zipkin.TagHTTPStatusCode.Set(span, strconv.Itoa(gc.Writer.Status()))
+			zipkin.TagHTTPResponseSize.Set(span, strconv.Itoa(gc.Writer.Size()))
 		}
 	}
 }
